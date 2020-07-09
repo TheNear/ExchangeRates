34e820ccbf8d1c45939d009a5d4162aa4e96572b
TheNear <thenearx@gmail.com>
2020-07-09 20:44:22 +0300

    Updates

---
+++ precache-manifest.289abf1eed6a0149ee07a4cf0924d492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1e8fa9eb41a6168aadae72d4a4073f",
+    "url": "/ExchangeRates/index.html"
+  },
+  {
+    "revision": "09bf7ff6d2fce4ac49b7",
+    "url": "/ExchangeRates/static/css/main.432f74c2.chunk.css"
+  },
+  {
+    "revision": "b2077aecb781945034a4",
+    "url": "/ExchangeRates/static/js/2.4a733545.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/ExchangeRates/static/js/2.4a733545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09bf7ff6d2fce4ac49b7",
+    "url": "/ExchangeRates/static/js/main.bff8790c.chunk.js"
+  },
+  {
+    "revision": "d1217a5956fed53b5996",
+    "url": "/ExchangeRates/static/js/runtime-main.0c5b01ed.js"
+  }
+]);
